f67b2c2214b94b0c3daa721cc68cc0187a324edb
Hana <2006yihui@gmail.com>
2026-05-07 12:59:51 +0800

    fix: update documentation

--- src/config/model-data.ts
+++ src/config/model-data.ts
@@ -1,5 +1,5 @@
 export const modelConfig = {
-  name: "ConvNextSmall + EfficientNet-b3 + ECA Ensemble",
+  name: "ConvNextSmall + EfficientNet-b3 + Rank Fusion Ensemble",
   version: "1.0.0",
   lastUpdated: "2026-04-24",
   inputSize: "224x224 px",
@@ -13,39 +13,39 @@ export const modelConfig = {
   },
   confusionMatrix: [
     [
-          439,
-          8,
-          1,
-          0,
-          0
-        ],
-        [
-          13,
-          70,
-          17,
-          1,
-          4
-        ],
-        [
-          10,
-          28,
-          204,
-          20,
-          15
-        ],
-        [
-          0,
-          0,
-          10,
-          32,
-          2
-        ],
-        [
-          0,
-          5,
-          13,
-          14,
-          82
-        ]
+      439,
+      8,
+      1,
+      0,
+      0
+    ],
+    [
+      13,
+      70,
+      17,
+      1,
+      4
+    ],
+    [
+      10,
+      28,
+      204,
+      20,
+      15
+    ],
+    [
+      0,
+      0,
+      10,
+      32,
+      2
+    ],
+    [
+      0,
+      5,
+      13,
+      14,
+      82
+    ]
   ]
 };
